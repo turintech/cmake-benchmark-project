fb69ec5c7926b977bea4c8d60e2de605b2e1e636
Rafail Giavrimis <47496212+grafail@users.noreply.github.com>
2024-01-11 14:29:34 +0000

    Update example.py

--- example.py
+++ example.py
@@ -6,7 +6,6 @@ np.random.seed(0)
 
 df = pd.DataFrame(np.random.randint(0,100000000,size=(100, 8)), columns=list('ABCDEFGH'))
 
-
 for index in df.index:
     if df['A'][index] > 38712131 and df['A'][index] < 58712131:
         print ("df[" + str(index) + "]['A']=" + str(df['A'][index]) + str(df['B'][index]))
