181373535dc81041e5ae889a36de54508b24612a
Rafail Giavrimis <47496212+grafail@users.noreply.github.com>
2024-01-11 13:09:28 +0000

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -6,7 +6,7 @@ std::string Reverse::reverse(std::string& toReverse)
 
     for(std::string::reverse_iterator rit=toReverse.rbegin(); rit!=toReverse.rend(); ++rit)
     {
-        ret.insert(ret.end(), *rit);
+        ret.insert(ret.end(), *rit); 
     }
     return ret;
 }
